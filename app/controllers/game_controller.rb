ecc9a4ba4d65d572d38aeff2ecd3eb8e9ea6b7c6
Nelson Bassey <nellybaz10@gmail.com>
2021-06-28 16:21:08 +0200

    update tic-tac-toe gem to 0.1.5

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -19,24 +19,12 @@ class GameController < ApplicationController
   end
 
 
-  # api/move
-
-
-  # validate a human move and return state of the game
-
-  # make a computer move and return state of the game
-  
-  # send a move and current board state
-
   def move
     board_state = params[:state]
     type = params[:type]
-    # move = params[:move]
     symbol = params[:symbol]
     response  = TicTacToe.move(type, board_state, symbol)
 
-    # move = 0
-    # move = type == 'computer' ? TicTacToe.computer_move(board_state, 1, 'X') : TicTacToe.smart_computer_move(board_state, 1, 'X')
     render json: response
   end
 end
